e1490e7e7cd66c53643566f226432712b8be1989
Tech-an <sato.tetsuya.c2@s.mail.nagoya-u.ac.jp>
2022-05-29 19:13:50 +0900

    firebaseの追加

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// 必要な関数を import
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBtrT43JDj-zuKUQduUaIPGnwobXr1MsbQ",
+  authDomain: "febro-original-team-app.firebaseapp.com",
+  projectId: "febro-original-team-app",
+  storageBucket: "febro-original-team-app.appspot.com",
+  messagingSenderId: "287197980810",
+  appId: "1:287197980810:web:f38cfc31d72d5e326f6573",
+  measurementId: "G-98T9KESG9M",
+}
+
+// Firebaseアプリオブジェクトを初期化
+const app = initializeApp(firebaseConfig)
+// Firestoreを読み込み、db(databaseの略)として export
+export const db = getFirestore(app)
