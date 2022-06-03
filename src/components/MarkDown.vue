af8ac86b3506599f33a41f979e456f8c31d434a3
Tech-an <sato.tetsuya.c2@s.mail.nagoya-u.ac.jp>
2022-06-03 15:42:12 +0900

    MarkDownエディターの試作

---
+++ src/components/MarkDown.vue
@@ -0,0 +1,37 @@
+<!-- 
+  学習用メモ
+
+  marked.js : (ライブラリ) MarkDownのテキストをHTMLに変換してくれるライブラリ
+  → 利用する場合、ターミナルで "npm install marked" と打ち、インストールする必要がある
+
+  v-html : (ディレクティブ) html形式の文章をコンパイルしてくれる 
+
+-->
+
+<template>
+  <form id="post-form">
+    <div id="editor">
+      <textarea v-model="markdownText">ここに入力するよ</textarea>
+    </div>
+    <div id="preview">
+      <div v-html="compiledMarkdown"></div>
+    </div>
+  </form>
+</template>
+<script>
+import marked from "marked"
+
+export default {
+  name: "postForm",
+  data() {
+    return {
+      markdownText: "",
+    }
+  },
+  computed: {
+    compiledMarkdown: () => marked(this.markdownText),
+  },
+}
+</script>
+
+<style></style>
