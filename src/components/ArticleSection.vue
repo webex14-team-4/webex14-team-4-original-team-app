e2853815e5340bebcdf95e10fe4c5b0602aa5f05
Tech-an <sato.tetsuya.c2@s.mail.nagoya-u.ac.jp>
2022-06-08 15:09:41 +0900

    firestoreのデータを取得し、管理者ページに表示できることを確認

--- src/components/ArticleSection.vue
+++ src/components/ArticleSection.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="article_section">
-    <div class="article_title">{{ title }}</div>
-    <div><router-link to="/edit">編集</router-link></div>
-    <div @click="delete_article">削除</div>
+    <div class="manager_article_title">{{ title }}</div>
+    <div class="manager_article_edit_button">
+      <router-link to="/edit">編集</router-link>
+    </div>
+    <div class="manager_article_delete_button" @click="delete_article">
+      削除
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   props: {
+    id: String,
     title: String,
+    category: String,
+    mdText: String,
+    code: String,
   },
   methods: {
     delete_article() {},
@@ -17,4 +25,15 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+.article_section {
+  display: flex;
+}
+.manager_article_title {
+  font-size: 1.2rem;
+}
+
+.manager_article_edit_button,
+.manager_article_delete_button {
+}
+</style>
--- src/views/ManagerHomeView.vue
+++ src/views/ManagerHomeView.vue
@@ -3,37 +3,46 @@
     <h1>管理者画面</h1>
     <div class="manager_info">管理者の情報</div>
     <div class="manager_article">
-      <div>記事一覧</div>
-      <div>ここにfirestoreから取得した記事データが反映される</div>
-      <ArticleSectionVue />
-      <!-- <ArticleSectionVue :title="" v-for="" /> -->
+      <h2>記事一覧</h2>
+      <ArticleSectionVue
+        :title="article.title"
+        v-for="article in articles"
+        :key="article"
+      />
     </div>
   </div>
 
-  <div class="test_firestore">
-    <!-- <button @click="post_db">firestoreにデータを送信!!!</button> -->
-    <button @click="get_db">firestoreのデータを受信!!!</button>
-  </div>
+  <div class="test_firestore"></div>
 </template>
 
 <script>
 import ArticleSectionVue from "@/components/ArticleSection.vue"
-import { collection, getDoc } from "firebase/firestore"
-import { db } from "../firebase"
+import { collection, getDocs } from "firebase/firestore"
+import { db } from "@/firebase"
 
 export default {
   components: {
     ArticleSectionVue,
   },
-  methods: {
-    get_db() {
-      console.log(getDoc(collection(db, "tetsuya-test-article-db")))
-    },
+  data() {
+    return {
+      articles: [],
+    }
+  },
+  created() {
+    getDocs(collection(db, "tetsuya-test-article-db")).then((docs) => {
+      docs.forEach((doc) => {
+        this.articles.push({ id: doc.id, ...doc.data() })
+      })
+    })
   },
 }
 </script>
 
 <style>
+.manager_article > h2 {
+  text-align: left;
+}
 .manager_info {
   background-color: rgb(224, 222, 222);
   margin-bottom: 50px;
