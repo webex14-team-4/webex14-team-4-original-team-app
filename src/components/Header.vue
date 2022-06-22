81ce9b1d333b524c57f6a4a16ae3d198cf0e644f
Tech-an <sato.tetsuya.c2@s.mail.nagoya-u.ac.jp>
2022-06-22 19:23:04 +0900

    Headerにコメントを記入（はると君への依頼用）

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,37 @@
+<!-- TODO 現在いるページに合わせて適切なヘッダーに変更する 
+
+【ヘッダーの仕様】
+
+〇ヘッダーは全3パターンで想定
+- パターン1. ログイン前ページ：TopPage
+- パターン2. ログイン関連ページ：LoginPage , SignUpPage
+- パターン3. ログイン後ページ：上記以外全てのページ
+
+〇全3パターンのヘッダータイトルとヘッダーメニューについて
+- パターン1の場合(TopPage). 
+  - ヘッダータイトル → AlgoLearn
+  - ヘッダーメニュー → ログイン , サインアップ
+
+- パターン2の場合(LoginPage , SignUpPage). 
+  - ヘッダータイトル → なし
+  - ヘッダーメニュー → なし
+
+- パターン3の場合(その他ページ). 
+  - ヘッダータイトル → AlgoLearn
+  - ヘッダーメニュー → ホーム , 一覧 , ログアウト
+
+〇実施手順
+1. masterからheaderブランチを切って移動
+2. methodのpath関数とcomputedのmenus関数の挙動を確認する
+3. 現在のパスの情報に応じて、v-ifを使ってパターン1,2,3を分岐するhtmlを書く
+4. 分岐先で現在のパスに対応したヘッダー内容が表示されるようにhtmlを書く
+5. 分岐先で適切なスタイルが適応されるようにcssを書く
+
+-->
+
 <template>
   <div class="header">
-    <router-link class="link subtitle" to="/">algoLearn</router-link>
+    <h1 class="link subtitle algo-color">AlgoLearn</h1>
     <div class="menu">
       <router-link
         class="link"
@@ -13,7 +44,6 @@
     </div>
   </div>
 </template>
-
 <script>
 export default {
   data() {
@@ -51,10 +81,20 @@ export default {
       },
     }
   },
+  methods: {
+    path() {
+      /** 現在のパスを返します
+       * 例：http://localhost:8080/index → index , http://localhost:8080/user/t5QaIsxhHHT0btDNyMMFJGFk2NL2 → user
+       */
+      return this.$route.path.split("/")[1]
+    },
+  },
   computed: {
     menus() {
-      const paths = this.$route.path.split("/")
-      return this.headerMenus[paths[1]]
+      /** 現在のパスに対応したheaderMenusで定義されたヘッダーメニューの「名前」と「リンク」のObjectを返します
+       * 例：pathがindex →[{ name: "一覧", link: "/index" },{ name: "ログイン", link: "/login" },]
+       */
+      return this.headerMenus[this.path()]
     },
   },
 }
@@ -73,6 +113,7 @@ export default {
 .header .link {
   text-decoration: none;
   color: black;
+  font-weight: normal;
 }
 .menu {
   display: flex;
@@ -80,5 +121,11 @@ export default {
 }
 .menu > * {
   margin: 0 20px;
+}
+
+.algo-color {
+  background: linear-gradient(90deg, #6ca7ff 0%, #d182fd 100%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
 }
 </style>
